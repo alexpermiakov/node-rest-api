1078c00679f0cb847a689754d3f4f31d3c84fb21
Alex Permyakov <alex.permyakoff@gmail.com>
2020-05-25 16:52:38 +0200

    use got for making requests

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
+import { getPlacesByName } from './SearchController';
 
 export default [
   {
-    path: '/',
+    path: '/api/v1/search',
     method: 'get',
-    handler: async (req: Request, res: Response) => {
-      res.send('Hello world!');
-    },
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q as string);
+        res.status(200).send(result);
+      },
+    ],
   },
 ];
