39c5cecbf1f1b38bfea23bea6fe9f6ab612cf6b4
Alex Permiakov <alex.permyakoff@gmail.com>
2020-07-25 14:26:58 +0200

    use axios instead of got

--- src/services/search/routes.test.ts
+++ src/services/search/routes.test.ts
@@ -1,14 +1,14 @@
 import express, { Router } from 'express';
 import request from 'supertest';
 import { applyMiddleware, applyRoutes } from '../../utils';
-import got from 'got';
+import axios from 'axios';
 import middleware from '../../middleware';
 import errorHandlers from '../../middleware/errorHandlers';
 import routes from '../../services/search/routes';
 
-jest.mock('got');
+jest.mock('axios');
 
-(got as any).mockResolvedValue({ body: '{"features": []}' });
+(axios as any).mockResolvedValue({ data: { features: [] } });
 
 describe('routes', () => {
   let router: Router;
