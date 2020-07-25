f18c5e9a8a3f7931d6b77025e081d986c06133e9
Alex Permiakov <alex.permyakoff@gmail.com>
2020-07-25 14:37:43 +0200

    ignore dist

--- src/services/search/providers/OpenCageDataProvider.ts
+++ src/services/search/providers/OpenCageDataProvider.ts
@@ -7,6 +7,5 @@ export const getPlaces = async (query: string) => {
   const key = process.env.OPEN_CAGE_DATA_KEY;
   const url = `https://api.opencagedata.com/geocode/v1/geojson?q=${query}&key=${key}&limit=20&no_annotations=1`;
   const response = await axios.get(url);
-  console.log(response);
   return response.data;
 };
